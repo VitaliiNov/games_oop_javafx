f5dfcebd04e42a25662326ed6ce462c42002a623
Admin <yaveselyi@yandex.ru>
2023-01-26 12:40:30 +0800

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
+++ puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
-public class WinTest {
+public class WinTest  {
     @Test
     public void whenVerticalWin() {
         int[][] board = {
